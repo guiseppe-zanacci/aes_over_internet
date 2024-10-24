f0eb94071ca5d03ebfe3cbd49b5e7355752cd768
Vtrixx <vidar.wicklund@hotmail.com>
2024-10-24 18:24:49 +0200

    Works

--- decryptor.py
+++ decryptor.py
@@ -1,14 +1,14 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import binascii
 
-key = binascii.unhexlify("DFC170B2F484BB16CEA0EE8FFF53E21F")  # Convert hex to bytes
-iv = binascii.unhexlify("7F2C02DE7B7EF2E879A12798232C21A6")    # Convert hex to bytes
+# Convert hex to bytes
+key = binascii.unhexlify("DFC170B2F484BB16CEA0EE8FFF53E21F")
+iv = binascii.unhexlify("7F2C02DE7B7EF2E879A12798232C21A6")
 
 def decrypt_message(encrypted_text, key, iv):
-
     # AES-128 decryption in CBC mode
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     decryptor = cipher.decryptor()
@@ -20,21 +20,17 @@ def decrypt_message(encrypted_text, key, iv):
     unpadder = padding.PKCS7(128).unpadder()
     plaintext = unpadder.update(decrypted_padded_message) + unpadder.finalize()
 
-    return plaintext.decode("utf-8")
-
+    return plaintext.decode('utf-8')  # Convert bytes back to string
 
 app = Flask(__name__)
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    data = request.data  # Decode incoming data
+    data = request.data  # Get raw binary data
     print("Received data from client")
-    print(decrypt_message(data, key, iv))
-    return
-
-
+    decrypted_message = decrypt_message(data, key, iv)
+    print(f"Decrypted Message: {decrypted_message}")
+    return 'Data received', 200
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)  # Listen on localhost at port 5000
-
-
--- encryptor.py
+++ encryptor.py
@@ -1,7 +1,6 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
-import os
 import requests
 import time
 import binascii
@@ -9,14 +8,17 @@ import binascii
 key = binascii.unhexlify("DFC170B2F484BB16CEA0EE8FFF53E21F")  # Convert hex to bytes
 iv = binascii.unhexlify("7F2C02DE7B7EF2E879A12798232C21A6")    # Convert hex to bytes
 
+
 def send_data(data):
+    count = 1
     while True:
         # Simulate sending some data
         try:
             response = requests.post('http://127.0.0.1:5000/upload', data=data)
-            print("Successfully sent")
+            print("Successfully sent " + str(count))
         except:
-            print("Error connecting")
+            print("Error connecting " + str(count))
+        count += 1
         time.sleep(2)  # Wait for 2 seconds before sending the next message
 
 
